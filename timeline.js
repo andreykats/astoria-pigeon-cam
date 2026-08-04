dbd7ecaece7579ce9718d9480f52e7c89753bbd7
Andrey Kats <andreykats@gmail.com>
2026-08-03 22:23:48 -0400

    Fill in placeholder timeline dates, update footer text

--- timeline.js
+++ timeline.js
@@ -1,7 +1,7 @@
 // Add a new nest event by appending a line at the bottom of this list:
 //   { date: "Month Day, Year", text: "What happened" },
 const TIMELINE_EVENTS = [
-  { date: "REPLACE_WITH_DATE", text: "Pigeons arrival" },
-  { date: "REPLACE_WITH_DATE", text: "Nest building has begun" },
-  { date: "REPLACE_WITH_DATE", text: "Eggs layed" }
+  { date: "March 10, 2026", text: "Pigeons arrival" },
+  { date: "March 15, 2026", text: "Nest building has begun" },
+  { date: "March 24, 2026", text: "Eggs layed" }
 ];
