3f9329f3aebdc635084e1f25599575f087b1beaf
Andrey Kats <andreykats@gmail.com>
2026-09-06 01:26:53 -0400

    Update live stream video ID

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Update this whenever you start a new YouTube broadcast.
 // Find the ID in the stream's URL: youtube.com/watch?v=THIS_PART
 const CONFIG = {
-  videoId: "Jbe3x30Nbt8"
+  videoId: "E3nBZLVQiRU"
 };
